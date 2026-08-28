32d7fb65dcd5a2e63271764ef4f8bac7fd93db62
Nikhil Prasad <contact.nikhilprasad@gmail.com>
2026-08-28 11:43:31 +0530

    feat(api): integrate async database audit logging into scan route (#53)
    feat(api): integrate async database audit logging into scan route by injecting database dependency and serializing pydantic models

--- src/api/scan.py
+++ src/api/scan.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.models import AuditLog
+from src.core import get_db
 from src.schemas import ScanRequest, DetectedEntity, ScanResponse
 from src.agent.graph import build_workflow
 import time
@@ -8,7 +11,7 @@ app_engine = build_workflow()
 scan_router = APIRouter()
 
 @scan_router.post('/scan', response_model= ScanResponse, tags= ["Scanner"], status_code= status.HTTP_200_OK)
-async def scan_pii(request: ScanRequest):
+async def scan_pii(request: ScanRequest, db: AsyncSession= Depends(get_db)):
 
      """
     Receives raw text, processes it through the LangGraph PII detection engine, 
@@ -25,6 +28,23 @@ async def scan_pii(request: ScanRequest):
 
           process_time_ms = int(time.time() - start_time) * 1000
 
+          raw_entities = result_state.get("detected_entities", [])
+
+          serialized_entities = [entitiy.model_dump() for entitiy in raw_entities]
+
+          new_audit_record = AuditLog(
+               original_text  = scan_text,
+               sanitized_text = result_state.get("sanitized_text", ""),
+               pii_detection  = serialized_entities,
+               processing_time= process_time_ms
+          )
+
+          db.add(new_audit_record)
+
+          await db.commit()
+
+          await db.refresh(new_audit_record)
+
           return {
                "original_text"     : scan_text,
                "sanitized_text"    : result_state.get("sanitized_text", ""),
@@ -33,6 +53,7 @@ async def scan_pii(request: ScanRequest):
           }
      
      except Exception as e:
+          await db.rollback()
           print(f"Error: {e}")
           raise HTTPException(
                status_code    = status.HTTP_500_INTERNAL_SERVER_ERROR,
